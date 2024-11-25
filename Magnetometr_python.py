87c81af442a71108e2d1850efb35f3bfef3d4152
ProjectFreakazoid <mo313112@student.polsl.pl>
2024-11-25 14:52:12 +0100

    Decoding values. Data as dict. Check if magnetometer properly sends data.

--- Magnetometr_python.py
+++ Magnetometr_python.py
@@ -1,12 +1,15 @@
+import sys
 import serial
+from fontTools.misc.cython import returns
 
 #definicja połączenia z portem szeregowym
-with serial.Serial('COM3', 115200, timeout=1) as dev:
+with serial.Serial('COM7', 115200, timeout=1) as dev:
     #wysłanie komendy do podania danych
     dev.write(bytearray([0x03,0,0,0,0,0]))
-
+    rxbit_all = [0]*6
+    rxbit_str = ""
+    i=0
     for x in range(31):
-        rxbit_str = ""
         #odczytywanie jednego bajta danych
         rx = dev.read(1)
         #konwertowanie jednego bajta danych na jego reprezentacje bitowom w formie stringa
@@ -16,14 +19,28 @@ with serial.Serial('COM3', 115200, timeout=1) as dev:
         if len(rxbit) < 8:
             for y in range(8-len(rxbit)):
                 rxbit_str += "0"
-            rxbit_str += rxbit
+            rxbit_str+=rxbit
         else:
-            rxbit_str = rxbit
+            rxbit_str+=rxbit
 
-        if (x)%6 == 0:
-            print("------")
-
-        print(rxbit_str)
+        if ((x+1) % 6 == 0 or x==30):
+            rxbit_all[i]= rxbit_str
+            rxbit_str=''
+            i+=1
+if rxbit_all[5]!='00001000':
+    print("Magnetometer do not reply correctly")
+    sys.exit()
+def magnetometer_value(z):
+    val = int(rxbit_all[z][16:], 2)
+    if rxbit_all[z][12] == "1":
+        val = -val
+    val = val / pow(10, int(rxbit_all[z][13:16], 2))
+    return val
+measurements = {'x': magnetometer_value(1), 'y': magnetometer_value(2), 'z':magnetometer_value(3), 'sum':magnetometer_value(4)}
+print(f'x = {measurements['x']}')
+print(f'y = {measurements['y']}')
+print(f'z = {measurements['z']}')
+print(f'sum = {measurements['sum']}')
 
 #zapis int liczby binarnej
 #decoding 2-go wiersza
